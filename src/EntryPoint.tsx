a2a4c259934c137017c3e30bb21b03458caa7107
Grazia Palombella <grazia.palombella@gmail.com>
2023-05-04 15:28:42 +0200

    develop: basic header

--- src/EntryPoint.tsx
+++ src/EntryPoint.tsx
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from './routes/Home';
 // import { Detail } from './routes/Detail';
 import { Pages } from './types';
+import { Header } from './components';
 
 
 const EntryPoint = () => {
   return (
     <>
+      <Header />
        <Routes>
           <Route path={Pages.Home} element={<Home />} />
 
