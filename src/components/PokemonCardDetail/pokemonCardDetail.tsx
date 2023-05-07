a2b4778869b70083d90c424b255f7e78bca69633
Grazia Palombella <grazia.palombella@gmail.com>
2023-05-07 12:42:48 +0200

    fix: css implementation

--- src/components/PokemonCardDetail/pokemonCardDetail.tsx
+++ src/components/PokemonCardDetail/pokemonCardDetail.tsx
@@ -20,46 +20,46 @@ const PokemonCardDetail: FC<PokemonCardDetailProps> = ({ pokemon }) => {
   const { height, weight, stats, types, moves } = pokemon
   return (
     <>
-      <button onClick={handleGoBack}>Back</button>
-      <div className="pokemon-card-detail">
+    <div className="pokemon-card-detail-container">
+      <button className="pokemon-card-detail-back" onClick={handleGoBack}>Back</button>
+      <div className="pokemon-card-detail-name">
         <h1>{pokemon.name}</h1>
         <img src={frontDefaultImage} alt={`image of: ${pokemon.name}`} />
       </div>
-      <>
-        <div>
-          <h2>Height:</h2>
+        <div className="pokemon-card-detail-height">
+          <h2>Height</h2>
           <label>{height}</label>
         </div>
-        <div>
-          <h2>Weight:</h2>
+        <div className="pokemon-card-detail-weight">
+          <h2>Weight</h2>
           <label>{weight}</label>
         </div>
-        <div>
-          <h2>Types:</h2>
+        <div className="pokemon-card-detail-types">
+          <h2>Types</h2>
           <label>
             {types?.map((type) => type.type.name).join(' ')}
           </label>
         </div>
-        <div>
+        <div className="pokemon-card-detail-stats-container">
           <h2>Stats</h2>
-          <label>
+          <div className='pokemon-card-detail-stats-wrapper'>
             {stats?.map((stat) => {
               return (
-                <div key={stat.stat.name}>
+                <div key={stat.stat.name} className='pokemon-card-detail-stat'>
                   <label>{stat.stat.name}</label>
-                  <progress value={stat.base_stat} max="100"></progress>
+                  <progress value={stat.base_stat} max="100" />
                 </div>
               )
             })}
-          </label>
+          </div>
         </div>
-        <div>
-          <h2>Moves:</h2>
+        <div className='pokemon-card-detail-moves'>
+          <h2>Moves</h2>
           <label>
             {moves?.map((move) => move.move.name).join(' ')}
           </label>
         </div>
-      </>
+    </div>
     </>
   )
 }
