6f0ecd58ee2cf5205b28a5ccfa864c19959e59ec
Grazia Palombella <grazia.palombella@gmail.com>
2023-05-08 18:42:12 +0200

    feat: add heart to pokemon card

--- src/components/PokemonCard/pokemonCard.tsx
+++ src/components/PokemonCard/pokemonCard.tsx
@@ -2,20 +2,34 @@ import React, { FC } from 'react'
 import './pokemonCard.css'
 import { Link } from 'react-router-dom'
 import { Pokemon } from '../../types'
+import { Button } from '../Button'
 
 export interface PokemonCardProps {
   readonly pokemon: Pokemon
+  readonly onFavourite: (pokemon: Pokemon) => void
 }
 
-const PokemonCard: FC<PokemonCardProps> = ({ pokemon }) => {
+const PokemonCard: FC<PokemonCardProps> = ({ pokemon, onFavourite }) => {
   const frontDefaultImage = pokemon.sprites?.front_default || 'img not found'
+
+  const handleFavourite = () => {
+    onFavourite(pokemon)
+  }
+
   return (
-    <Link to={`/pokemon/${pokemon.name}`} className="pokemon-card-link">
-      <div className="pokemon-card">
-        <h1>{pokemon.name}</h1>
-        <img src={frontDefaultImage} alt={`image of: ${pokemon.name}`} />
-      </div>
-    </Link>
+    <div className="pokemon-card-container">
+      <Link to={`/pokemon/${pokemon.name}`} className="pokemon-card-link">
+        <div className="pokemon-card">
+          <h1>{pokemon.name}</h1>
+          <img src={frontDefaultImage} alt={`image of: ${pokemon.name}`} />
+        </div>
+      </Link>
+      <Button
+        icon={`./favourite-heart.png`}
+        className="pokemon-card-favourite"
+        onClick={handleFavourite}
+      />
+    </div>
   )
 }
 
