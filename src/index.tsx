47b82e6072cca4772e5fa8f9b2962d66483067e8
Grazia Palombella <grazia.palombella@gmail.com>
2023-05-08 13:53:55 +0200

    fix: seems to be finally deploying correctly 🤯

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root') as Element)
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename={document.baseURI.substring(document.baseURI.indexOf(window.location.origin) + window.location.origin.length, document.baseURI.lastIndexOf('/'))}>
       <EntryPoint />
     </BrowserRouter>
   </React.StrictMode>
